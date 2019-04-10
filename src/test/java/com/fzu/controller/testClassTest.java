c2643c565b631c917114804da39b8011f880ae7c
BruceweeeJ <18506904188@163.com>
2019-04-10 10:54:21 +0800

    添加测试文件testClassTest

---
+++ src/test/java/com/fzu/controller/testClassTest.java
@@ -0,0 +1,24 @@
+package com.fzu.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class testClassTest {
+
+    @Test
+    public void selectAll() {
+    }
+
+    @Test
+    public void show() {
+    }
+
+    @Test
+    public void addAccount() {
+    }
+
+    @Test
+    public void login() {
+    }
+}
